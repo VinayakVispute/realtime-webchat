1aba524fe9aa6aad6cedc0e93b8f9d6b22f997c7
Vinayak Vispute <vinayakvispute4@gmail.com>
2024-02-19 02:09:25 +0530

    Fix:Import Bug Fixed

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -1,4 +1,4 @@
-const { createRoom } = require("../controllers/RoomController");
+const { createRoom } = require("../controllers/roomController");
 const express = require("express");
 const router = express.Router();
 

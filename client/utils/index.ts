bed56bfbaa358cf4d19919620315d97156e0c09e
Vinayak Vispute <vinayakvispute4@gmail.com>
2024-08-19 03:18:04 +0530

    Testing

--- client/utils/index.ts
+++ client/utils/index.ts
@@ -5,7 +5,7 @@ export const getInitials = (name: string | undefined) => {
     .join("");
 };
 
-// This is testingsf edf
+// testing
 
 export const isObjectEmpty = (obj: Object): boolean => {
   return Object.keys(obj).length === 0;
